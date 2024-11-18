468c302e00ff8f4efd1607186bc143c7b982af48
romanrusetzky1243192 <roman1243192@gmail.com>
2024-11-18 15:25:20 +0700

    настройка экземпляра http клиента, устанавливаем таймаут подключения чтения и записи

--- app/src/main/java/com/example/marsnasa1/retrofit/Marsnasa1Retrofitinstance.kt
+++ app/src/main/java/com/example/marsnasa1/retrofit/Marsnasa1Retrofitinstance.kt
@@ -5,15 +5,20 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class Marsnasa1Retrofitinstance {
     private const val BASE_URL = "https://api.nasa.gov/"
-
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
     private val client = OkHttpClient.Builder()
-        .addInterceptor(logging)        .build()
+        .addInterceptor(logging)
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .build()
+
     val api: Marsnasa1Api by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
