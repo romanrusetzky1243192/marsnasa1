fec2ff08cda1195855a16e74230d11ee5d068e58
romanrusetzky1243192 <roman1243192@gmail.com>
2024-11-18 15:58:25 +0700

    добавил экран в MainActivity для отображения

--- app/src/main/java/com/example/marsnasa1/MainActivity.kt
+++ app/src/main/java/com/example/marsnasa1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.marsnasa1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +12,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.marsnasa1.screen.Marsnasa1Screen
 import com.example.marsnasa1.ui.theme.Marsnasa1Theme
 
 class MainActivity : ComponentActivity() {
     private val apiKey: String ="ключ"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            Log.d("MainActivity", "Setting content")
+            Marsnasa1Screen(apiKey, sol = 1122, date = "2024-10-01")
+        }
     }
 }
