613139b690593fcea3897873c086e915026a9ee6
romanrusetzky1243192 <roman28682972@gmail.com>
2024-11-22 14:01:16 +0500

    добавил возможность сохранения в галерею изображения

--- app/src/main/java/com/example/marsnasa1/screen/Marsnasa1Screen.kt
+++ app/src/main/java/com/example/marsnasa1/screen/Marsnasa1Screen.kt
@@ -1,21 +1,30 @@
 package com.example.marsnasa1.screen
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.marsnasa1.viewModel.Marsnasa1ViewModel
 import androidx.compose.ui.res.painterResource
+import androidx.core.graphics.drawable.toBitmap
+import coil.imageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.example.marsnasa1.R
+import com.example.marsnasa1.saveimg.saveImageToGallery
 import com.example.marsnasa1.ui.theme.Purple40
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -26,6 +35,7 @@ fun Marsnasa1Screen(apiKey: String, sol: Int, date: String?) {
     val likedStates = remember { mutableStateListOf<Boolean>() }
     val commentsList = remember { mutableStateListOf<MutableList<String>>() }
     val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope() // Добавлено
 
     LaunchedEffect(Unit) {
         viewModel.fetchPhotos(apiKey, "Curiosity", sol, date)
@@ -91,6 +101,39 @@ fun Marsnasa1Screen(apiKey: String, sol: Int, date: String?) {
                                 .fillMaxWidth()
                                 .height(300.dp)
                         )
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        val context = LocalContext.current
+                        Button(onClick = {
+                            scope.launch {
+                                try {
+                                    val request = ImageRequest.Builder(context)
+                                        .data(photo.img_src)
+                                        .build()
+
+                                    val result = (context.imageLoader.execute(request) as SuccessResult)
+                                    val bitmap = result.drawable.toBitmap()
+
+                                    if (bitmap != null) {
+                                        Log.d("MarsRoverScreen", "Изображение загружено успешно, начинается сохранение...")
+
+                                        val fileName = "MarsRover_${System.currentTimeMillis()}"
+                                        saveImageToGallery(context, bitmap, fileName)
+
+                                        Log.d("MarsRoverScreen", "Изображение успешно сохранено в галерею под именем: $fileName")
+                                        Toast.makeText(context, "Фотография успешно сохранена в галерее!", Toast.LENGTH_LONG).show()
+                                    } else {
+                                        Log.e("MarsRoverScreen", "Не удалось загрузить изображение: bitmap равен null")
+                                    }
+                                } catch (e: Exception) {
+                                    Log.e("MarsRoverScreen", "Ошибка при загрузке изображения: ${e.message}")
+                                    Toast.makeText(context, "Ошибка при загрузке изображения: ${e.message}", Toast.LENGTH_LONG).show()
+                                }
+                            }
+                        }) {
+                            Text("Сохранить в галерею")
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
 
                         IconButton(onClick = {
                             likedStates[page] = !likedStates[page]
