139aa3538c8edee87651573d0bfa9716b9b84dbe
romanrusetzky1243192 <roman1243192@gmail.com>
2024-12-07 00:09:35 +0700

    Revert "создал и добавил свою иконку приложения"
    This reverts commit 524fe4a77a9a33fdddd68c6c5fd6014cd83c38cd.

--- app/src/main/java/com/example/marsnasa1/saveimg/saveImageToGallery.kt
+++ app/src/main/java/com/example/marsnasa1/saveimg/saveImageToGallery.kt
@@ -1,57 +1,24 @@
 package com.example.marsnasa1.saveimg
 
-import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
-import android.os.Build
 import android.os.Environment
-import android.provider.MediaStore
 import android.widget.Toast
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import java.io.OutputStream
 
 fun saveImageToGallery(context: Context, bitmap: Bitmap, imageName: String) {
-    val resolver = context.contentResolver
+    val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    val file = File(storageDir, "$imageName.jpg")
 
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-
-        val contentValues = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, "$imageName.jpg")
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MarsRover")
+    try {
+        FileOutputStream(file).use { out ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+            Toast.makeText(context, "Изображение сохранено в галерею!", Toast.LENGTH_SHORT).show()
         }
-
-
-        val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
-        imageUri?.let {
-            try {
-                val outputStream: OutputStream? = resolver.openOutputStream(it)
-                outputStream?.use { stream ->
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-                    stream.flush()
-                    Toast.makeText(context, "Изображение сохранено в галерею!", Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-                Toast.makeText(context, "Ошибка при сохранении изображения!", Toast.LENGTH_SHORT).show()
-            }
-        }
-    } else {
-
-        val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val file = File(storageDir, "$imageName.jpg")
-        try {
-            FileOutputStream(file).use { out ->
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-                Toast.makeText(context, "Изображение сохранено в галерею!", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            Toast.makeText(context, "Ошибка при сохранении изображения!", Toast.LENGTH_SHORT).show()
-        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+        Toast.makeText(context, "Ошибка при сохранении изображения!", Toast.LENGTH_SHORT).show()
     }
 }
